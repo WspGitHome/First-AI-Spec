10cd5b81d05cf541b4a61e7d8ef3b3c57081b6dc
wushuping <wusp@smartsteps.com>
2026-01-04 18:23:50 +0800

    second add

--- src/main/java/com/example/geminispringboot/GeminiSpringbootApplication.java
+++ src/main/java/com/example/geminispringboot/GeminiSpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeminiSpringbootApplication {
 
     //first commit !
+    //second
     public static void main(String[] args) {
         SpringApplication.run(GeminiSpringbootApplication.class, args);
     }
