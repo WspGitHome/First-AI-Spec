433c5564a0c94488bb84b04ae47d30df3f7b37b3
wushuping <wusp@smartsteps.com>
2026-01-04 18:21:45 +0800

    提交注释 --fist add

--- src/main/java/com/example/geminispringboot/GeminiSpringbootApplication.java
+++ src/main/java/com/example/geminispringboot/GeminiSpringbootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeminiSpringbootApplication {
 
+    //first commit !
     public static void main(String[] args) {
         SpringApplication.run(GeminiSpringbootApplication.class, args);
     }
